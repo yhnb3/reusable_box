890a55b8438176e075125b49be5068801779d197
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-06-16 18:46:41 +0900

    Design : Select 디자인 씌우기

--- src/components/Selection/OptionItem/index.tsx
+++ src/components/Selection/OptionItem/index.tsx
@@ -1,12 +1,15 @@
 import { MouseEvent } from 'react'
+import cx from 'classnames'
+
 import { useSelectionContext } from '../hooks'
+import styles from './optionItem.module.scss'
 
 interface IProps {
   value: string
 }
 
 const OptionItem = ({ value }: IProps) => {
-  const { select } = useSelectionContext()
+  const { selected, select } = useSelectionContext()
 
   const onClick = (event: MouseEvent<HTMLButtonElement>) => {
     const { name } = event.currentTarget.dataset
@@ -14,7 +17,12 @@ const OptionItem = ({ value }: IProps) => {
   }
 
   return (
-    <button type='button' data-name={value} onClick={onClick}>
+    <button
+      className={cx(styles.optionItem, { [styles.selected]: selected === value })}
+      type='button'
+      data-name={value}
+      onClick={onClick}
+    >
       {value}
     </button>
   )
--- src/components/Selection/Trigger/index.tsx
+++ src/components/Selection/Trigger/index.tsx
@@ -1,10 +1,11 @@
 import { useSelectionContext } from '../hooks/useSelectionContext'
+import styles from './trigger.module.scss'
 
 const Trigger = () => {
   const { click, selected } = useSelectionContext()
   return (
-    <button type='button' onClick={click}>
-      {selected}
+    <button type='button' onClick={click} className={styles.trigger}>
+      <div>{selected}</div>
     </button>
   )
 }
